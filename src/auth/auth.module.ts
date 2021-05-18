8c5cf718363d8326308e90db1fa18da7a7497287
Shivanshu <shivanshu@triveous.com>
2021-05-18 15:45:01 +0530

    v

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './jwt-strategy';
     JwtModule.register({
        secret: process.env.JWT_SECRET || "trvsSecretKey",
        signOptions: {
-          expiresIn : parseInt(process.env.JWT_EXPIRE)
+          expiresIn : process.env.JWT_EXPIRE
        }
     }),
    TypeOrmModule.forFeature([UserRepository])

c609eb349f6da265ffa537be8e935822bfbf07ba
Shivanshu <shivanshu@triveous.com>
2021-05-18 12:34:45 +0530

    added .env

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,16 +1,15 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import * as config from 'config';
+const { DATABASE_URL, DATABASE_PORT, DATABASE_USER, DATABASE_PASS, DATABASE_NAME, DATABASE_HOST } = process.env;
 
-const dbConfig = config.get('db');
 
 export const typeOrmConfig : TypeOrmModuleOptions = {
-   type : dbConfig.type,
-   host : "ec2-23-23-128-222.compute-1.amazonaws.com",
+   type : "postgres",
+   host : DATABASE_HOST,
    port : 5432,
-   username : "urvtjyxnjyslis",
-   password : "304588cb68571ceca2b4afabc5b8ed182f18d166089f1c53b8ffdc94263af685",
-   database : "d10m2rbia660po",
-   synchronize : process.env.TYPE_ORM_SYNC || dbConfig.synchronise,
+   username : DATABASE_USER,
+   password : DATABASE_PASS,
+   database : DATABASE_NAME,
+   synchronize : true,
    autoLoadEntities : true
 
 }

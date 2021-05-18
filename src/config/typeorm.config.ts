3613e20636249190e27f96d912a333d88b709a1a
Shivanshu <shivanshu@triveous.com>
2021-05-18 08:24:48 +0530

    Added configurations

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,13 +1,16 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 export const typeOrmConfig : TypeOrmModuleOptions = {
-   type : 'postgres',
-   host : 'localhost',
-   port : 5432,
-   username : 'postgres',
-   password : 'postgres',
-   database : 'todoapp',
-   synchronize : true,
+   type : dbConfig.type,
+   host : process.env.HOST_NAME || dbConfig.host,
+   port : process.env.PORT_NAME || dbConfig.port,
+   username : process.env.USERNAME || dbConfig.username,
+   password : process.env.PASSWORD || dbConfig.password,
+   database : process.env.DATABASE || dbConfig.database,
+   synchronize : process.env.TYPE_ORM_SYNC || dbConfig.synchronise,
    autoLoadEntities : true
 
 }

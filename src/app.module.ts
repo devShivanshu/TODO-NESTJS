841385b8c2b54022417498a843bbac24bfade118
Shivanshu <shivanshu@triveous.com>
2021-05-18 12:35:05 +0530

    b

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TodosModule } from './todos/todos.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from 'config';
 
 @Module({
-  imports: [TodosModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
+  imports: [TodosModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule, ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  })],
 
 })
 export class AppModule {}
